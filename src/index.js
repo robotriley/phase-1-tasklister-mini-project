6dea0136582ecdcdc1cab87cbb123cdbda9de9f6
riley soloner <rileysoloner@gmail.com>
2022-06-29 11:37:08 -0400

    first three deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const formSubmit = document.querySelector("#create-task-form")
+  formSubmit.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const createListItem = () => {
+      const userTextBox = document.querySelector("#new-task-description")
+      const userText = userTextBox.value
+      let li = document.createElement('li')
+      li.innerText = userText
+      const tasksList = document.querySelector("#tasks")
+      tasksList.append(li)
+    }
+    createListItem();
+  })
+}); 
+
