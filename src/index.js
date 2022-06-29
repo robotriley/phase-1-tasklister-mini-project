0bc096901b67132a108ba1ffbb4e1cd224411de2
riley soloner <rileysoloner@gmail.com>
2022-06-29 14:25:17 -0400

    added dropdown menu

--- src/index.js
+++ src/index.js
@@ -6,12 +6,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const createListItem = () => {
       const userTextBox = document.querySelector("#new-task-description")
       const userText = userTextBox.value
-      let li = document.createElement('li')
+      const li = document.createElement('li')
       li.innerText = userText
       const tasksList = document.querySelector("#tasks")
       tasksList.append(li)
+      const dropdown = document.createElement("select") //dropdown menu selector
+      dropdown.setAttribute("class", "dropdownMenu")
+      li.prepend(dropdown)
+      const highPriority = document.createElement("option")
+      highPriority.innerText = "High"
+      highPriority.value = "high"
+      const medPriority = document.createElement("option")
+      medPriority.innerText = "Medium"
+      medPriority.value = "medium"
+      const lowPriority = document.createElement("option")
+      lowPriority.innerText = "Low"
+      lowPriority.value = "low"
+      dropdown.append(highPriority, medPriority, lowPriority)
+      li.setAttribute("class", "highPrio")
     }
     createListItem();
+
+    const changeMenu = document.querySelector('.dropdownMenu');
+  
+    changeMenu.addEventListener('change', (event) => {
+      console.log("menu changed")
+      changeMenu.parentElement.className = ""
+    })
   })
 }); 
 
